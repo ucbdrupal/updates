f49f9e7373e9f82fdad9f7bd4e0afc52d022d8ca
bwood <bwood@berkeley.edu>
2012-08-20 12:07:18 -0700

    Add test switch.  Show last cron in watchdog.

--- updates.php
+++ updates.php
@@ -13,34 +13,44 @@
 require_once("lib_util.inc");
 include_once("updates_aliases_pantheon_v1.inc");
 include_once("updates_aliases_pantheon.inc");
+
 // VARIABLES
 $drush = exec('which drush');
 $drush_up = 'pm-update --pipe --security-only';
 $egrep = exec('which egrep');
+
+$test_mode = FALSE;
+
+$test_sites = array('pantheon.hrweb.live');
+
 // FUNCTIONS
 
 // MAIN
 
 // Command Line Options
-$shortopts = "h";
+$shortopts = "ht";
 $longopts = ""; // not supported in < php 5.3
 $options = getopt($shortopts);
 $usage = "
 USAGE:
 
-Required arguments:
+php update.php
+
+Provides a report of pending updates for the drush aliases
+listed in the update*.inc files
 
 Optional arguments:
+
+-t    Run script on just the test site ($test_site)
 
 ";
 
 
 foreach (array_keys($options) as $o) {
   switch ($o) {
-    /*
-     case '':
-     $opt[''];
-     */
+    case 't':
+      $test_mode = TRUE;
+      break;
     case 'h':
       print $usage;
       exit(1);
@@ -59,18 +69,24 @@ msg("Working...\n");
 
 //TODO: Make sure they have correct drush alias files
 
-$supported_sites = array_merge($supported_sites, $supported_sites_v1);
+if ($test_mode) {
+  msg("Test mode", TRUE);
+  $supported_sites = $test_sites;
+}
+else {
+  $supported_sites = array_merge($supported_sites, $supported_sites_v1);
+}
 
-//TODO: check if cron is running
 
 foreach ($supported_sites as $alias) {
   $name = doexec($drush . ' @' . $alias . ' vget site_name');
   $name = str_replace('site_name: "', '', trim($name['out'][0], '"'));
   $cmd_result[$name]['basics'] = doexec($drush . ' @' . $alias . ' ' . "status | $egrep \"Drupal version|Site URI\"");
+  $cmd_result[$name]['cron'] = doexec($drush . ' @' . $alias . ' ' . "ws --count=1 \"cron\"");
   $cmd_result[$name]['updates'][] = doexec($drush . ' @' . $alias . ' ' . $drush_up);
 }
 
-//print_r($cmd_result); 
+//print_r($cmd_result);
 $num_sites_total = count($cmd_result);
 $num_sites_with_updates = 0;
 ksort($cmd_result);
@@ -81,6 +97,10 @@ while(list($k, $v) = each($cmd_result)) {
     msg($out);
   }
   print "\n";
+  print "Last cron run:\n";
+  foreach($v['cron']['out'] as $out) {
+    msg($out);
+  }
   $num_updates_site = count($v['updates'][0]['out']) - 1; //there's always a blank line at the end
   if ($num_updates_site > 0) {
     $num_sites_with_updates++;
